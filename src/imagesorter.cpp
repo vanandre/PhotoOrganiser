6cf78384a8f7c14c41a2a21992c4646a4df8a285
vanandre <vanoncini@hotmail.com>
2020-02-13 09:57:28 +0100

    Replacing of filename is working, now the paths have to be concatenated together and the file has to be renamed

--- src/imagesorter.cpp
+++ src/imagesorter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "imagesorter.hpp"
 
+namespace fs = std::filesystem;
 
 ImageSorter::ImageSorter(std::string dir_loc) {
     m_dir_path = dir_loc;
@@ -12,9 +13,19 @@ ImageSorter::~ImageSorter() {
 }
 
 void ImageSorter::sort_files() {
-    for (auto const &entry : std::filesystem::directory_iterator(m_dir_path)) {
+    int it = 1;
+    for (auto const &entry : fs::directory_iterator(m_dir_path)) {
         if (entry.path().extension() == ".png") {
             std::cout << "Found image" << std::endl;
+
+            fs::path dir_path = entry.path().parent_path();
+            std::string new_name = "image_" + std::to_string(it) + ".png";
+
+            // Read exif info and rename the file
+            fs::path new_path = entry.path().filename().replace_filename(new_name);
+            std::cout << new_path.filename() << std::endl;
+            // std::filesystem::rename(entry.path(), new_path);
+            it++;
         }
     }
 }
