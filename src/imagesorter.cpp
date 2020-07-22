8b04d6428e28e3df94d68cf27f8d10f37ce70501
André Vanoncini <vanoncini@hotmail.com>
2020-07-22 23:13:11 +0200

    Some changes

--- src/imagesorter.cpp
+++ src/imagesorter.cpp
@@ -15,15 +15,18 @@ ImageSorter::~ImageSorter() {
 void ImageSorter::sort_files() {
     int it = 1;
     for (auto const &entry : fs::directory_iterator(m_dir_path)) {
+        std::string parent_path = m_dir_path += std::string("/");
         if (entry.path().extension() == ".png") {
             std::cout << "Found image" << std::endl;
 
-            fs::path dir_path = entry.path().parent_path();
             std::string new_name = "image_" + std::to_string(it) + ".png";
 
             // Read exif info and rename the file
-            fs::path new_path = entry.path().filename().replace_filename(new_name);
-            std::cout << new_path.filename() << std::endl;
+            fs::path new_path = parent_path += entry.path().filename().replace_filename(new_name);
+            if (!new_path.is_absolute()) {
+                new_path.parent_path() = m_dir_path;
+            }
+            std::cout << new_path << std::endl;
             // std::filesystem::rename(entry.path(), new_path);
             it++;
         }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <thread>
+#include <iostream>
 #include "imagesorter.hpp"
 
-int main( ) {
-    ImageSorter sorter("/home/vanandre/Pictures/");
+
+int main() {
+    std::string folder_path = "/home/vanandre/Pictures";
+    // std::cout << "Folder Path: " << std::endl;
+    // std::cin >> folder_path;
+    ImageSorter sorter(folder_path);
     sorter.sort_files();
     return 1;
 }
